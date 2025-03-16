29f1fb6c90b1e5312aed1eab328efd16e61c5a26
imvmanish19 <imvmanish19@gmail.com>
2025-03-16 13:04:08 +0530

    Implemented Task Service

---
+++ task-management/user-service/cmd/task-service.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+    "task-service/internal/handler"
+    "github.com/gin-gonic/gin"
+)
+
+func main() {
+    router := gin.Default()
+    
+    router.POST("/tasks", handler.CreateTask)
+    router.GET("/tasks", handler.GetTasks)
+    router.PUT("/tasks/:id", handler.UpdateTask)
+    router.DELETE("/tasks/:id", handler.DeleteTask)
+
+    router.Run(":8080")
+}
